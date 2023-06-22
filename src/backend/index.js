4972e7a10f51a3555bcf2ec1148285379c8a4766
natali09yarmolik <85163434+natali09yarmolik@users.noreply.github.com>
2023-06-22 21:30:15 +0300

    fix: change backend file

--- src/backend/index.js
+++ src/backend/index.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3010
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    /*res.header("Access-Control-Allow-Origin", "http://localhost:3000");*/
+    res.header("Access-Control-Allow-Origin", "https://natali09yarmolik.github.io");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
